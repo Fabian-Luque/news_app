ef9f297b64972849fe2426df849a2fb40623f53a
Fabian-Luque <Fabianluque2014@gmail.com>
2020-11-23 08:21:14 -0300

    first commit

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: BouncingScrollPhysics(),
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+          ),
+
+          Container(
+            color: Colors.green,
+          )
+        ],
+      )
+    );
+  }
+}
